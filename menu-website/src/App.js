843272ae193a082a9068ae74cd5f7fa13b82b86a
Robert Goniszewski <robertgoniszewski@outlook.com>
2020-07-10 05:53:43 +0200

    menu-website/: adding 'create-react-app' with 'post-css' and 'tailwindcss'
    Signed-off-by: Robert Goniszewski <robertgoniszewski@outlook.com>

---
+++ menu-website/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import Main from "./pages/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
