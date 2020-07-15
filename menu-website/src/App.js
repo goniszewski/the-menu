51e375499b8646e9710ac668a8e29241320aacfe
Robert Goniszewski <robertgoniszewski@outlook.com>
2020-07-15 11:27:57 +0200

    Adding 'react-router-dom' and configuring the '/editor' path

--- menu-website/src/App.js
+++ menu-website/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Main from "./pages/Main";
+import Editor from "./pages/MenuEditor";
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path exact="/">
+            <Main />
+          </Route>
+          <Route path="/editor">
+            <Editor />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- menu-website/src/pages/MenuEditor.js
+++ menu-website/src/pages/MenuEditor.js
@@ -2,7 +2,9 @@ import React from "react";
 import { DragDropContext } from "react-beautiful-dnd";
 
 class MenuEditor extends React.Component {
-  render() {}
+  render() {
+    return <div>Editor says: Hi!</div>;
+  }
 }
 
 export default MenuEditor;
